79b89f2e8de904c6f518d075bf1e5ccbd22eb185
JC <jcdotnet@hotmail.com>
2026-04-17 12:58:44 +0200

    feat :: update education and jobs timeline

--- curriculum/src/shared/data.js
+++ curriculum/src/shared/data.js
@@ -1,5 +1,15 @@
 export const BACKGROUND = {
-	education: ["Ingeniería Técnica en Informática (Universidad de Málaga)"],
-	courses: ['2006. Desarrollo de aplicaciones en J2EE (Forman, PTA Málaga)', '2007. MCTS (.NET 2.0, Forman, PTA Málaga)', '2012. English B1 certificate (EOI Málaga)', '2017. Full Stack Web Development (Hong Kong University, Online)'],
-	jobs: ['Ene 2016 - Dic 2024. Programador y desarrollador de software por cuenta propia']
+	education: [
+		'Ingeniería Técnica en Informática (Universidad de Málaga)'
+	],
+	courses: [
+		'2016 - Actualidad. Formación continua en .NET/C#+ y tecnologías modernas',
+    	'2012. English B1 certificate (EOI Málaga)',
+        '2007. MCTS (.NET 2.0, Forman, PTA Málaga)',
+        '2006. Desarrollo de aplicaciones en J2EE (Forman, PTA Málaga)'
+	],
+	jobs: [
+		'Ene 2016 - Mar 2025. Ingeniero de Software Independiente (Consultoría y Desarrollo)',
+		'Jun 2014 - Dic 2014. Lead .NET Developer en Bolt Accelerator (Startup en incubación)'
+	]
 }
