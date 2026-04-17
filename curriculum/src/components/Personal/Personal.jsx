273316ffdb4d3c72662df81ad29a99a2f4995071
JC <jcdotnet@hotmail.com>
2026-04-17 12:34:52 +0200

    refactor :: update personal header and profile links

--- curriculum/src/components/Personal/Personal.jsx
+++ curriculum/src/components/Personal/Personal.jsx
@@ -6,7 +6,7 @@ export default function Personal() {
 	return <div className={classes.Personal}>
 		<img src={jc} alt="JC" className={classes.Avatar} />
 		<h1> José Carlos Román Rubio </h1>
-		<h2> Software developer </h2>
+		<h2> Software Engineer </h2>
 		<ul>
 			<li>
 				<svg className={classes.Icon}><use xlinkHref={Sprite + '#icon-location-pin'}></use></svg>
@@ -14,17 +14,17 @@ export default function Personal() {
 			</li>
 			<li>
 				<svg className={classes.Icon}><use xlinkHref={Sprite + '#icon-mail'}></use></svg>
-				<a href="mailto:info@josecarlosroman.com">info@josecarlosroman.com</a>
+				<a href="mailto:contact@josecarlosroman.com">contact@josecarlosroman.com</a>
 			</li>
 			<li>
-				<svg className={classes.Icon}><use xlinkHref={Sprite + '#icon-twitter'}></use></svg>
-				<a href="https://twitter.com/jcdev_">https://twitter.com/jcdev_</a>
+				<svg className={classes.Icon}><use xlinkHref={Sprite + '#icon-linkedin'}></use></svg>
+				<a href="https://www.linkedin.com/in/romanrubio/">in/romanrubio</a>
 			</li>
 		</ul>
 		<p>
 			<svg className={classes.Icon} style={{ verticalAlign: 'sub' }}><use xlinkHref={Sprite + '#icon-code'}></use></svg>
 			Esta página web se ha creado con <strong><a href="https://react.dev/" target="_blank" rel="noreferrer">React</a></strong>.
-			Código: <strong><a href="https://github.com/jcdotnet/curriculum">GitHub</a></strong>.
+			Código: <strong><a href="https://github.com/jcdotnet/react-lab/tree/main/curriculum">GitHub</a></strong>.
 		</p>
 	</div>
 }
